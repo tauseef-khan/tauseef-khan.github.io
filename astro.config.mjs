112c377629dabda0df05ed075fc4cc20afaf5c49
tauseef-khan <tdakhan@hotmail.co.uk>
2024-07-04 05:40:04 +0100

    change to single quotes to see if it fixes 404 error

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "static",
+  output: 'static',
   adapter: vercel(),
   site: 'https://tauseefkhan.xyz'
 });

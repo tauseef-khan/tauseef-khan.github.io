3db3407c99f99b3017bb0949adbbddc078eea9ca
tauseef-khan <tdakhan@hotmail.co.uk>
2024-07-04 05:36:57 +0100

    fix to resolve 404 code being returned on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
+  output: "static",
   adapter: vercel(),
   site: 'https://tauseefkhan.xyz'
 });

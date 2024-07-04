fe87f4dff0f5b823c0a3483af02b7d1d7afb1596
tauseef-khan <tdakhan@hotmail.co.uk>
2024-07-04 06:13:29 +0100

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tauseef-khan.github.io',
+  build:{
+    site: 'https://tauseef-khan.github.io/',
+  },
   integrations: [tailwind(), icon(), compress()],
   output: "static",
   adapter: node({

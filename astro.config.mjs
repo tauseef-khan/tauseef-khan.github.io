d2c3466f76aa4816d9225b98c4d206e8a9f8f51e
tauseef-khan <tdakhan@hotmail.co.uk>
2024-07-04 13:07:14 +0100

    trying 'static' output again

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://tauseefkhan.xyz',
   integrations: [tailwind(), icon(), compress()],
-  output: "hybrid",
+  output: "static",
   adapter: vercel()
 });

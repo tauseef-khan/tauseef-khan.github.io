dbf812796396e4ba6c423810a76f4703dfd5ef26
tauseef-khan <tdakhan@hotmail.co.uk>
2024-07-04 06:04:44 +0100

    might aswell try it

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tauseef-khan.github.io',
+  base: '/src/pages',
   integrations: [tailwind(), icon(), compress()],
   output: "static",
   adapter: vercel()

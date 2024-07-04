5570850999e1bd1fba260bc06152c5801b764ccc
tauseef-khan <tdakhan@hotmail.co.uk>
2024-07-04 05:56:23 +0100

    going back to github page url to debug issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tauseefkhan.xyz',
+  site: 'https://tauseef-khan.github.io',
   integrations: [tailwind(), icon(), compress()],
   output: "static",
   adapter: vercel()
